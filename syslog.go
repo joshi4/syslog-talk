9d1729eac03ed62fc2d343ea4e92495ca5ee60b6
Shantanu <joshishantanu4@gmail.com>
2016-07-12 12:03:40 -0600

    backup code branch

--- syslog.go
+++ syslog.go
@@ -1,19 +1,20 @@
 package main
 
 import (
+	"crypto/tls"
 	"io"
 	"log"
-	"log/syslog"
 	"os"
 	"time"
 )
 
 func main() {
 	// START OMIT
-	sw, err := syslog.Dial("tcp", "logs4.papertrailapp.com:27193", 0, "gophercon2016")
+	sw, err := tls.Dial("tcp", "logs4.papertrailapp.com:27193", nil)
 	if err != nil {
 		log.Println(err)
 	} else {
+		log.SetPrefix("gophercon2016: ")
 		log.SetOutput(io.MultiWriter(sw, os.Stderr))
 	}
 	//END OMIT
